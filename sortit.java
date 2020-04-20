d910c9ab8ab76ff7ccbbfa643f0b565e9631b4b9
Gerald-Wheaton <geraldwheaton2000@gmail.com>
2020-04-20 16:54:03 -0400

    making output better

--- sortit.java
+++ sortit.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
 import java.util.LinkedList; 
 import java.util.Queue;
@@ -11,23 +10,22 @@ public class sortit {
         Queue<String> list0 = new LinkedList<String>();
         Queue<String> list1 = new LinkedList<String>();
         Queue<String> currQ = new LinkedList<String>();
-        Queue<String> ulteriorQ = new LinkedList<String>();
         
         Scanner input = new Scanner(System.in);
         String newItem = "", rear = "", userInput = "";
-        int sameList; //integer boolean.
-        //Scanner input = new Scanner(System.in);
+        String currentFront = "", alternateFront = ""; //representing the front of the current queue and also the second queue
+        int list0Length, list1Length;
 
         currQ = list0; //initializing currQ
         if (args.length != 0) { //collect all filenames from args array
             for (int i = 0; i < args.length; i++) { //collects user input from given files or manual entry
             //below lines get user input
                 if (args[i].equals("-")) {
-                    while(!userInput.equals("q")) {
+                    while(!userInput.equals("q") && !userInput.equals("Q")) {
                         System.out.print("Enter a word or 'q' to exit: ");
                         userInput = input.nextLine();
 
-                        if(!userInput.equals("q") && !userInput.equals("")) {
+                        if(!userInput.equals("q") && !userInput.equals("") && !userInput.equals("Q")) {
                             //currQ = (newItem.compareTo(rear) < 0) ? list0 : list1; //ternary operatot to switch lists when newItem is less than the rear elemnt of currQ
                             if (newItem.compareTo(rear) < 0) {
                                 if(currQ == list0) {
@@ -70,12 +68,11 @@ public class sortit {
                 }
             } 
         }
+        input.close(); //closing Scanner
 
 
         /* -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
         //Queue sorting process
-        String back = "", currentFront = "", alternateFront = ""; //representing the front of the current queeu and also the second Queue
-        int list0Length, list1Length;
 
         //end loop once one queue is empty
         while(list0.size() != 0 && list1.size() != 0) {
@@ -190,17 +187,10 @@ public class sortit {
             }
         }
 
-        System.out.println();
-        System.out.println();
-        while(list0.size() != 0) {
-            System.out.print(list0.remove() + " ");
-        }
+        currQ = (list0.size() == 0) ? list1 : list0;
 
-        System.out.println();
-
-        while(list1.size() != 0) {
-            System.out.print(list1.remove() + " ");
+        while(currQ.size() != 0) {
+            System.out.println(list0.remove() + " ");
         }
-        System.out.println();
     }
 }
