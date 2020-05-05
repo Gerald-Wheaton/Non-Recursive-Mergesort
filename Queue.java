fd1364581fd859adb37726361a9e8aff78acc368
Gerald-Wheaton <geraldwheaton2000@gmail.com>
2020-05-05 12:33:59 -0400

    first commit

--- Queue.java
+++
@@ -1,5 +0,0 @@
-public class Queue {
-    LinkedQueue();
-    boolean isEmpty();
-    
-}
--- sJ.bat
+++
@@ -1,8 +0,0 @@
-java sortit ..\SortIt\321.txt
-java sortit ..\SortIt\3221.txt
-java sortit ..\SortIt\32221.txt
-java sortit ..\SortIt\3222231.txt
-java sortit ..\SortIt\322231.txt
-java sortit ..\SortIt\empty.txt
-java sortit -
-java sortit ..\SortIt\321.txt - ..\SortIt\321.txt
